e04d8f32fb5d11e8d9ee44a5db41d0217136f232
ffespechen <flavio.espechenieva@gmail.com>
2021-06-10 18:17:41 -0300

    credenciales para db4free

--- config/db.js
+++ config/db.js
@@ -3,18 +3,13 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config({ path: 'variables.env' });
 
 // Option 2: Passing parameters separately (other dialects)
-const db = new Sequelize(
-  process.env.BD_NOMBRE,
-  process.env.BD_USER,
-  process.env.BD_PASS,
-  {
-    host: process.env.BD_HOST,
-    port: process.env.BD_PORT,
-    dialect: 'mysql',
-    define: {
-      timestamps: false,
-    },
-  }
-);
+const db = new Sequelize('uptasknode', 'ffespechennodejs', 'ixefo515DB4FREE', {
+  host: 'db4free.net',
+  port: 3306,
+  dialect: 'mysql',
+  define: {
+    timestamps: false,
+  },
+});
 
 module.exports = db;
